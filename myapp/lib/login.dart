49ba421a179fe160734985a9ae3f5b7352ca1def
Khosbayar <bhosoo0523@gmail.com>
2024-11-11 23:08:11 +0900

    divided to widgets login page

--- myapp/lib/login.dart
+++ myapp/lib/login.dart
@@ -34,45 +34,77 @@ class LoginPage extends StatelessWidget {
                 ],
               ),
               Theme.of(context).componentPadTop,
-              Expanded(
-                child: Column(
-                  children: [
-                    Text("Username"),
-                    Theme.of(context).componentPadTop,
-                    TextFormField(
-                      decoration: InputDecoration(hintText: "Username"),
-                    ),
-                    Theme.of(context).componentPadTop,
-                    Text("Password"),
-                    Theme.of(context).componentPadTop,
-                    TextFormField(
-                      decoration: InputDecoration(hintText: "Password"),
-                    ),
-                    Theme.of(context).componentPadTop,
-                    Row(
-                      children: [
-                        Checkbox(value: false, onChanged: null),
-                        Text("Remember me"),
-                        ElevatedButton(onPressed: () {}, child: Text("login"))
-                      ],
-                    ),
-                    Theme.of(context).componentPadTop,
-                    Row(
-                      children: [
-                        ElevatedButton(onPressed: () {}, child: Text("G")),
-                        SizedBox(width: 30,),
-                        ElevatedButton(onPressed: () {}, child: Text("F")),
-                      ],
-                    ),
-                  ],
-                ).smallRoundedContainer().largeRoundedContainer(),
-              ),
+              LoginSection(),
               // Expanded(child: TextFormField(decoration: InputDecoration(hintText: "Username"),)
               // Text("data").baseRoundedContainer(),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class LoginSection extends StatelessWidget {
+  const LoginSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 50,),
+          Text(
+            "Username",
+            textAlign: TextAlign.left,
+            style: Theme.of(context).textTheme.displayMedium,
+          ),
+          Theme.of(context).componentPadTop,
+          TextFormField(
+            decoration: InputDecoration(hintText: "Username"),
+          ),
+          Theme.of(context).componentPadTop,
+          Text("Password",
+              textAlign: TextAlign.left,
+              style: Theme.of(context).textTheme.displayMedium),
+          Theme.of(context).componentPadTop,
+          TextFormField(
+            decoration: InputDecoration(hintText: "Password"),
+          ),
+          Theme.of(context).componentPadTop,
+          Row(
+            children: [
+              Checkbox(value: false, onChanged: null),
+              Text("Remember me"),
+              ElevatedButton(onPressed: () {}, child: Text("login"))
+            ],
+          ),
+          Theme.of(context).componentPadTop,
+          LoginMethods(),
+        ],
+      ).smallRoundedContainer().largeRoundedContainer(),
+    );
+  }
+}
+
+class LoginMethods extends StatelessWidget {
+  const LoginMethods({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        ElevatedButton(onPressed: () {}, child: Text("G")),
+        SizedBox(
+          width: 30,
+        ),
+        ElevatedButton(onPressed: () {}, child: Text("F")),
+      ],
     );
   }
 }
